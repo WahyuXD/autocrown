64c814660dc26594b4941387d9528d262681c5bf
Lukarishima Kinda SuS <92640904+Lukarishima@users.noreply.github.com>
2022-06-15 22:18:48 +0700

    Add files via upload

---
+++ stumble.py
@@ -0,0 +1,47 @@
+# Ingatt!! ini tools hanya fake tidak benar benar bisa menambahkan crown 
+import time
+import os
+import sys
+
+os.system("clear")
+
+print(''' 
+        -----------------------------------
+                  Auto Add Crown
+                Stumble Guys (Fake)
+        ----------------------------------- 
+               By Lukarishima
+            ::::::::::::::::::::::    ''')
+            
+
+user = input("Nickname : ")
+
+print(" {1} Crown")
+print(" {2} Thropy  (ComingSoon)")
+operasi = input("{1} atau {2} :")
+
+if operasi not in("1",):
+	print("Closed...")      
+	end
+
+if operasi == "1":
+	print("\033[93m")
+	print ("[                    ] 0% ")
+time.sleep(5)
+print ("[=====               ] 25%")
+time.sleep(5)
+print ("[==========          ] 50%")
+time.sleep(5)
+print ("[===============     ] 75%")
+time.sleep(5)
+print ("[====================] 100%")
+time.sleep(3)
+os.system("clear")
+x = 1
+while True:
+    print("Sukses Menambahkan  %d Crown" % (x))
+    time.sleep(0.2)
+    x += 1 
+    
+    
+
